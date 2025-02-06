7ae2e3175fce8c72d07407d0d51406303b0ace37
thehwayne <thehwayne@gmail.com>
2025-02-06 12:06:32 -0500

    12:06 changes

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:first_app/centered.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  GradientContainer(this.startAlignment, this.endAlignment,{super.key});
+  Alignment startAlignment;
+  Alignment endAlignment;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(
+          begin: startAlignment,
+          end: endAlignment,
+          colors: const [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 9, 1, 24),
+          ],
+        ),
+      ),
+      child: Centered("Welcome ITEC 315"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 12, 1, 39),
-                Color.fromARGB(255, 63, 8, 165),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white, 
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(Alignment.topLeft, Alignment.bottomRight),
       ),
     ),
   );
